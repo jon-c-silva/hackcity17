6059f2cfb3140f2079e95c490c5d481a6967aee8
jonathancardoso <jonathancardoso@outlook.com>
2017-03-25 18:25:21 +0000

    BikeShop app support

--- app/__init__.py
+++ app/__init__.py
@@ -5,82 +5,56 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-from app import views, models
+from app.models import BikeShop
 
-bike_shops = [
-    {
-		'id': 1,
-		'name': u'East Central Cycles',
-		'address=': u'18 Exmouth Market, Clerkenwell, London EC1R 4QE',
-		'latitude' : 51.5254231,
-		'longitude' : -0.1097451
-    },
-    {
-		'id': 2,
-		'title': u'FULLCITY CYCLES',
-		'address': u'72 Leather Ln, Clerkenwell, London EC1N 7TR',
-		'latitude' : 51.5210264,
-		'longitude' : -0.1097559
-    }
-]
 
-@app.route('/bike-shops', methods=['GET'])
-def get_bike_shops():
-    return jsonify({'bike_shops': bike_shops})
+@app.route('/bikeShops', methods=['GET'])
+def get_bikeShops():
+    return jsonify(json_list=[bs.serialize for bs in BikeShop.query.all()])
 
-@app.route('/bike-shops/<int:bike_shop_id>', methods=['GET'])
-def get_bike_shop(bike_shop_id):
-	bike_shop = [bike_shop for bike_shop in bike_shops if bike_shop['id'] == bike_shop_id]
-	if len(bike_shop) == 0:
-		abort(404)
-	return jsonify({'bike_shop': bike_shop[0]})
+@app.route('/bikeShops/<int:bikeShopId>', methods=['GET'])
+def get_bikeShop(bikeShopId):
+    bikeShop = BikeShop.query.get(bikeShopId)
+    print(bikeShop)
+    return jsonify({'bikeShop': bikeShop.serialize})
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.route('/bike-shops', methods=['POST'])
-def create_bike_shop():
-	if not request.json or not all(field in request.json for field in ['name', 'address', 'latitude', 'longitude']):
-		abort(400)
-	bike_shop = {
-		'id': bike_shops[-1]['id'] + 1,
-		'name': request.json['name'],
-		'address': request.json.get('address'),
-		'latitude': request.json.get('latitude'),
-		'longitude': request.json.get('longitude')
-	}
-	bike_shops.append(bike_shop)
-	return jsonify({'bike_shop': bike_shop}), 201
+@app.route('/bikeShops', methods=['POST'])
+def create_bikeShop():
+    if not request.json or not all(field in request.json for field in ['name', 'latitude', 'longitude']):
+        abort(400)
 
-@app.route('/bike-shops/<int:bike_shop_id>', methods=['PUT'])
-def update_bike_shop(bike_shop_id):
-	bike_shop = [bike_shop for bike_shop in bike_shops if bike_shop['id'] == bike_shop_id]
-	if len(bike_shop) == 0:
-		abort(404)
-	if not request.json:
-		abort(400)
-	if 'name' in request.json and not isinstance(request.json['name'],str):
-		abort(400)
-	if 'address' in request.json and not isinstance(request.json['address'],str):
-		abort(400)
-	if 'latitude' in request.json and not isinstance(request.json['latitude'],float):
-		abort(400)
-	if 'longitude' in request.json and not isinstance(request.json['longitude'],float):
-		abort(400)
-	bike_shop[0]['name'] = request.json.get('name', bike_shop[0]['name'])
-	bike_shop[0]['address'] = request.json.get('address', bike_shop[0]['address'])
-	bike_shop[0]['latitude'] = request.json.get('latitude', bike_shop[0]['latitude'])
-	bike_shop[0]['longitude'] = request.json.get('longitude', bike_shop[0]['longitude'])
-	return jsonify({'bike_shop': bike_shop[0]})
+    bikeShop = BikeShop(
+        name=request.json.get('name'), address=request.json.get('address',''),
+        postcode=request.json.get('postcode',''),website=request.json.get('website',''),
+        phone=request.json.get('phone',''),latitude=request.json.get('latitude'),
+        longitude=request.json.get('longitude'))
 
-@app.route('/bike_shops/<int:bike_shop_id>', methods=['DELETE'])
-def delete_bike_shop(bike_shop_id):
-	bike_shop = [bike_shop for bike_shop in bike_shops if bike_shop['id'] == bike_shop_id]
-	if len(bike_shop) == 0:
-		abort(404)
-	bike_shops.remove(bike_shop[0])
-	return jsonify({'result': True})
+    db.session.add(bikeShop)
+    db.session.commit()
+    return jsonify({'bikeShop': bikeShop.serialize}), 201
+
+@app.route('/bikeShops/<int:bikeShopId>', methods=['PUT'])
+def update_bikeShop(bikeShopId):
+    bikeShop = BikeShop.query.get(bikeShopId)
+    bikeShop.name = request.json.get('name', bikeShop.name)
+    bikeShop.latitude = request.json.get('latitude', bikeShop.latitude)
+    bikeShop.longitude = request.json.get('longitude', bikeShop.longitude)
+    bikeShop.address = request.json.get('address', bikeShop.address)
+    bikeShop.phone = request.json.get('phone', bikeShop.phone)
+    bikeShop.website = request.json.get('website', bikeShop.website)
+    db.session.commit()
+    return jsonify({'bikeShop': bikeShop.serialize})
+
+@app.route('/bikeShops/<int:bikeShopId>', methods=['DELETE'])
+def delete_bikeShop(bikeShopIdd):
+    bikeShop = BikeShop.query.get(bikeShopId)
+    db.session.delete(bikeShop)
+    db.session.commit()
+    return jsonify({'result': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/models.py
+++ app/models.py
@@ -1,11 +1,25 @@
 from app import db
 
 class BikeShop(db.Model):
-    latitude = db.Column(db.Float, primary_key=True)
-    longitude = db.Column(db.Float, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
     name = db.Column(db.String(120), index=True, unique=True)
-    website = db.Column(db.String(120), index=True, unique=True)
+    address = db.Column(db.String(120), index=True)
+    phone = db.Column(db.String(10), index=True)
+    website = db.Column(db.String(120), index=True)
     postcode = db.Column(db.String(7), index=True)
 
-    def __repr__(self):
-        return '<Bike shop %r>' % (self.name)
+    @property
+    def serialize(self):
+        """Return object data in serializable format"""
+        return {
+            'id'        : self.id,
+            'latitude'  : self.latitude,
+            'longitude' : self.longitude,
+            'name'      : self.name,
+            'address'   : self.address,
+            'phone'     : self.phone,
+            'website'   : self.website,
+            'postcode'  : self.postcode
+        }
