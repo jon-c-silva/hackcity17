b3ca966fb30c864d8bf217453f298d1d49569641
jonathancardoso <jonathancardoso@outlook.com>
2017-03-25 19:07:40 +0000

    create database

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__) + "/data")
+print(basedir)
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'fixmybike.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
